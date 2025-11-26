afe963ae56a3370133085e3caaba20c4bc8abccd
Sinem Havan <github-hesabini-actigin-havansinem@gmail.com>
2025-11-26 23:43:36 +0300

    Yorumlar temizlendi

--- app_api/models/venue.js
+++ app_api/models/venue.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-// 1. Saatler (Hour) Şeması
+
 var hour = new mongoose.Schema({
     days: { type: String, required: true },
     open: String,
@@ -8,7 +8,7 @@ var hour = new mongoose.Schema({
     isClosed: { type: Boolean, required: false }
 });
 
-// 2. Yorumlar (Comment) Şeması (Senin kodda burası eksikti)
+
 var comment = new mongoose.Schema({
     author: { type: String, required: true },
     rating: { type: Number, required: true, min: 0, max: 5 },
@@ -16,16 +16,17 @@ var comment = new mongoose.Schema({
     date: { type: Date, default: Date.now }
 });
 
-// 3. Ana Mekan (Venue) Şeması
+
 var venue = new mongoose.Schema({
     name: { type: String, required: true },
     address: String,
     rating: { type: Number, min: 0, max: 5, default: 0 },
     foodanddrink: [String], 
     coordinates: { type: [Number], index: '2dsphere' },
-    hours: [hour],       // Yukarıdaki hour şemasını kullanır
-    comments: [comment]  // Yukarıdaki comment şemasını kullanır
+    hours: [hour],      
+
+    comments: [comment] 
 });
 
-// Şemayı modele çeviriyoruz
+
 mongoose.model("venue", venue, "venues");
